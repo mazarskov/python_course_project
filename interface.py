839e53ecbd5bf3a361a5dc466cda431afa9dbe36
mazarskov <maxazarskov@gmail.com>
2023-11-29 17:54:20 +0200

    Added some GUI elements. Only exit button works.

--- interface.py
+++ interface.py
@@ -1,1 +1,38 @@
 #used for clean project front-end code
+
+import customtkinter as ctk
+from functions import *
+
+
+ctk.set_appearance_mode("light")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+app = ctk.CTk()  # create CTk window like you do with the Tk window
+app.title("Hotel Booking Management System (Early build)")
+app.geometry("1000x800")
+ 
+
+frame = ctk.CTkFrame(app, width=950, height=200, corner_radius=5)
+frame.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
+
+exit_button = ctk.CTkButton(app, text="Exit", command=exit_app, width=200)
+exit_button.place(relx=0.87, rely=0.9, anchor=ctk.CENTER) 
+
+name_input_field = ctk.CTkEntry(frame, placeholder_text="Type name here", width=300)
+name_input_field.place(relx = 0.13, rely = 0.2, anchor=ctk.W)
+name_label = ctk.CTkLabel(frame, text="Name", fg_color="transparent", font=('Arial', 15))
+name_label.place(relx = 0.05, rely = 0.2, anchor=ctk.W)
+
+country_input_field = ctk.CTkEntry(frame, placeholder_text="Type country here", width=300)
+country_input_field.place(relx = 0.57, rely = 0.2, anchor=ctk.W)
+country_label = ctk.CTkLabel(frame, text="Country", fg_color="transparent", font=('Arial', 15))
+country_label.place(relx = 0.5, rely = 0.2, anchor=ctk.W)
+
+pass_input_field = ctk.CTkEntry(frame, placeholder_text="Type passport number here", width=300)
+pass_input_field.place(relx = 0.13, rely = 0.4, anchor=ctk.W)
+pass_label = ctk.CTkLabel(frame, text="Passport", fg_color="transparent", font=('Arial', 15))
+pass_label.place(relx = 0.05, rely = 0.4, anchor=ctk.W)
+
+
+
+app.resizable(False, False)
+app.mainloop() #line that MUST be at the end of the code
