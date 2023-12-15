f34e08495ef70000ab78f87df5807b409a0d4c02
mazarskov <maxazarskov@gmail.com>
2023-12-15 16:20:45 +0200

    Added function to check the date format

--- functions.py
+++ functions.py
@@ -1,16 +1,17 @@
 #used for clean project back-end code
 import csv
 import os
+from datetime import datetime
 
 def exit_app():
     exit()
 
 def generate_text(user_name, user_gender, user_nationality, user_pass_num, user_housing, user_from_country, user_to_country):
-    text = f"\nName: {user_name}\nGender: {user_gender}\nNationality: {user_nationality}\nPassport_num: {user_pass_num}\nHousing option: {user_housing}\nFlying from: {user_from_country}\nFlying to: {user_to_country}"
+    text = f"\nName: {user_name}\nGender: {user_gender}\nCountry: {user_nationality}\nPassport_num: {user_pass_num}\nHousing option: {user_housing}\nDate from: {user_from_country}\nDate to: {user_to_country}"
     return text
 
 def generate_csv(name, gender, nationality, pass_num, housing, from_country, to_country):
-    header = ["Name", "Gender", "Nationality", "Pass_Num", "Housing", "Flying_from", "Flying_to"]
+    header = ["Name", "Gender", "Country", "Pass_Num", "Housing", "Date_from", "Date_to"]
     csv_values = [[name, gender, nationality, pass_num, housing, from_country, to_country]]
     filename = "test_output.csv"
     file_empty = not os.path.exists(filename) or not os.path.getsize(filename)
@@ -20,3 +21,13 @@ def generate_csv(name, gender, nationality, pass_num, housing, from_country, to_
         if file_empty:
             csvwriter.writerow(header)
         csvwriter.writerows(csv_values)
+
+
+def is_valid_date(date_string):
+    try:
+        # Attempt to parse the input string as a date in the DD/MM/YYYY format
+        datetime.strptime(date_string, '%d/%m/%Y')
+        return True
+    except ValueError:
+        # If parsing fails, the input is not in the correct format
+        return False
